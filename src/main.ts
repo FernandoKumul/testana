a8f28dce4e0d237ea0e5791a04bf8f293b4a603d
FernandoKumul <ferdesarrollador11@outlook.com>
2024-03-25 17:34:32 -0500

    init commit 2

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
+/* eslint-disable vue/no-reserved-component-names */
+/* eslint-disable vue/multi-word-component-names */
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config';
+import "primevue/resources/themes/aura-light-green/theme.css";
+import Button from 'primevue/button';
 
 const app = createApp(App)
-
+app.use(PrimeVue);
 app.use(router)
+app.component('Button', Button)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+const message = () => {
+  alert('Buuuuu')
+}
 </script>
 
 <template>
   <main>
     <img src="https://pm1.aminoapps.com/6224/c034a973c4a2e92d1738cbcf90dac3d255eb87a9_hq.jpg" alt="elmo">
+    <Button @click="message">Haz click :D</Button>
   </main>
 </template>
 
@@ -13,6 +17,8 @@ main {
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  gap: 8px
 }
 
 img {
