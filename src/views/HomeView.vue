c46119ca55dbed04c9db3e06219befe91e66f437
FernandoKumul <ferdesarrollador11@outlook.com>
2024-03-25 17:32:51 -0500

    init commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <img src="https://pm1.aminoapps.com/6224/c034a973c4a2e92d1738cbcf90dac3d255eb87a9_hq.jpg" alt="elmo">
+  </main>
+</template>
+
+<style>
+main {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+img {
+  width: 500px;
+}
+</style>
