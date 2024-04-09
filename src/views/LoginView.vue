c0c6df5d88233d8fc5f581a5a05c7e4e79cee7fe
AnaTMorales <moralesgarciaanateresa@gmail.com>
2024-04-08 23:24:50 -0500

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,56 @@
 <template>
-    <div class="about">
-      <h1>This is an Login page</h1>
+  <body>
+    <div class="column_1">
+      <h1>¡Bienvenido de nuevo!</h1>
+      <h1>(～￣w￣)～</h1>
+      
     </div>
+
+    <div class="column_2">
+      <h2>Iniciar Sesión</h2>
+      <div class="container">
+        <!-- <label for="">Mail</label>
+        <input type="mail" placeholder="Mail"> -->
+
+        <FloatLabel>
+          <InputText id="email" v-model="email"/>
+          <label for="email">Email</label>
+        </FloatLabel>
+
+        
+        <FloatLabel>
+          <InputText id="password" v-model="password"/>
+          <label for="password">Password</label>
+        </FloatLabel>
+
+        <Button label="Iniciar"/>
+        <RouterLink to="register">
+          ¿Aún no tienes una cuenta? Registrate
+        </RouterLink>
+
+      </div>
+    </div>
+
+    
+  </body>
   </template>
+
+  <script setup>
+  import Button from 'primevue/button';
+  import InputText from 'primevue/inputtext';
+  import FloatLabel from 'primevue/floatlabel';
+
+
+</script>
   
   <style>
+
+  body{
+    display: flex;
+  }
+
+  button{
+
+  }
   </style>
   
