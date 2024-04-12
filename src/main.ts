5eb0ef1bd213fbe3784803b2106452792dcc7dab
FernandoKumul <ferdesarrollador11@outlook.com>
2024-04-12 02:59:21 -0500

    Register and Login finished

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,17 @@ import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
 // import "primevue/resources/themes/aura-light-green/theme.css";
-import Button from 'primevue/button';
 import './assets/theme/aura-light/theme.css'
 import ToastService from 'primevue/toastservice';
+import Button from 'primevue/button';
+import Divider from 'primevue/divider';
 
 
 const app = createApp(App)
 app.use(PrimeVue);
 app.use(router)
 app.component('Button', Button)
+app.component('Divider', Divider)
 app.use(ToastService);
 
 app.mount('#app')
