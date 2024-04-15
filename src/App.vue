3188d9bde022fb8475422d28b25894a4e5f43aae
FernandoKumul <ferdesarrollador11@outlook.com>
2024-04-15 09:34:22 -0500

    Incluye Navbar and Footer

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
 import Toast from 'primevue/toast';
 import NavbarComponent  from "./components/NavbarComponent.vue";
 import FooterComponent  from "./components/FooterComponent.vue";
+import { useRoute } from 'vue-router';
 
+// Obtener el objeto route actual
+const route = useRoute();
+const viewForNavBar = ['home'];
+const viewForFooter = ['home']
+// Extraer la ruta actual del objeto route
 </script>
 
 <template>
-  <NavbarComponent></NavbarComponent>
+  <NavbarComponent v-if="viewForNavBar.includes(route.name?.toString() ?? '')"></NavbarComponent>
   <RouterView />
   <Toast />
-  <FooterComponent></FooterComponent>
+  <FooterComponent v-if="viewForFooter.includes(route.name?.toString() ?? '')"></FooterComponent>
 </template>
 
 <style scoped>
