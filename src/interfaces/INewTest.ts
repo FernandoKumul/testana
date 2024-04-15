1cf00e28a278373583465d2f1a49303d5e0d3790
FernandoKumul <ferdesarrollador11@outlook.com>
2024-04-14 22:13:02 -0500

    Now custom button need the clas btn-custom

---
+++ src/interfaces/INewTest.ts
@@ -0,0 +1,30 @@
+export interface INewTest {
+  userId: number
+  title: string
+  description: string
+  color: 'green' | 'blue' | 'purple' | 'orange' | 'yellow' | 'red'
+  visibility: 'public' | 'private' | 'unlisted'
+  image: string | null
+  status: boolean
+  random: boolean
+  duration: number | null
+  evaluateByQuestion: boolean
+  tags: string
+  questions: INewQuestion[]
+}
+
+export interface INewQuestion {
+  QuestionTypeId: number
+  description: string
+  image: string | null
+  order: number
+  points: number
+  duration: number | null
+  answers: INewAnswer[]
+  CaseSensitivity?: boolean
+}
+
+export interface INewAnswer {
+  text: string
+  correct: boolean
+}
--- src/views/CreateTestView.vue
+++ src/views/CreateTestView.vue
@@ -1,13 +1,88 @@
 <template>
-    <div class="about">
-      <h1>This is an create test page</h1>
-    </div>
-  </template>
+  <nav>
+    <Button @click="createTest">Crear</Button>
+  </nav>
+  <main>
+    <section >
+      <article class="card">
+        <div>
+          <label for="username">Título</label>
+          <InputText v-model="newTest.title" type="text" placeholder="Test sin título" />
+          <small id="username-help">El título es obligatorio</small>
+        </div>
+        <div>
+          <label for="username">Descripción</label>
+          <Textarea v-model="newTest.description" placeholder="Habla un poco sobre tu test" rows="3" />
+          <small id="username-help">La descripción es obligatoria</small>
+        </div>
+      </article>
+
+      <div>
+        <h3>0 preguntas</h3> 
+        <Button outlined>Nueva Pregunta</Button>
+      </div>
+    </section>
+
+    <aside class="card">
+      Aqui van las configuraciones
+    </aside>
+  </main>
+</template>
 
 <script lang="ts" setup>
+import { reactive } from 'vue';
+import type { INewTest } from '@/interfaces/INewTest';
+import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
 
+const newTest = reactive<INewTest>({
+  userId: 0,
+  title: '',
+  description: '',
+  color: 'orange',
+  visibility: 'public',
+  image: null,
+  status: false,
+  random: false,
+  duration: null,
+  evaluateByQuestion: true,
+  tags: '',
+  questions: []
+})
+
+
+const createTest = () => {
+  console.log(newTest)
+  //Subir
+}
 </script>
-  
-  <style scoped>
-  </style>
-  
+
+<style scoped>
+nav {
+  background-color: #FFDECC;
+  height: 60px;
+}
+
+textarea {
+  resize: none;
+}
+
+small {
+  color: #f87171;
+}
+
+main {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  column-gap: 20px;
+  padding: 48px 80px;
+}
+
+.card {
+  padding: 32px;
+  border-radius: 8px;
+  -webkit-box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.14);
+  -moz-box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.14);
+  box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.14);
+}
+</style>
