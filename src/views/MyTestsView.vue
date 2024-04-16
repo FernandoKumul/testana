701a24a366aae4fced452cff396064d0f05774b5
Noemi-Un <124269534+Noemi-Un@users.noreply.github.com>
2024-04-16 18:21:46 -0500

    Update MyTestsView.vue

--- src/views/MyTestsView.vue
+++ src/views/MyTestsView.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="main">
     <h1>Test que has creado</h1>
-    <TabMenu :model="items" @change="handleTabChange" />
+    <TabMenu :model="items" :activeIndex="tabId"  @tab-change="handleTabChange" />
     <div class="grid-search">
-      <div v-for="test in filteredTests" :key="test.Id">
+      <div v-for="test in tests" :key="test.Id">
         <Card style="width: 25rem; overflow: hidden;">
           <template #header>
-            <img :src="test.Image" alt="Test Image" class="card-image" />
+            <img :src="test.image" alt="Test Image" class="card-img" />
           </template>
-          <h2>{{ test.Title }}</h2>
-          <p>Autor: {{ test.AuthorName }}</p>
+                <template #title>{{ test.title }}</template>
+                <template #subtitle>{{ test.author }}</template>
         </Card>
       </div>
     </div>
-    <Paginator :rows="6" :totalRecords="filteredTests.length" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 import axios from 'axios';
-import Paginator from 'primevue/paginator';
 import Card from 'primevue/card';
-import TabMenu from 'primevue/tabmenu';
+import TabMenu, { type TabMenuChangeEvent } from 'primevue/tabmenu';
+
+const tabId = ref(0)
 
 interface Test {
   Id: number;
@@ -37,28 +37,29 @@ const items = ref([
   { label: 'Contestados' },
   { label: 'Creados' }
 ]);
-const selectedTab = ref('Contestados'); // Inicialmente se selecciona la pestaña "Contestados"
 
 const tests = ref<Test[]>([]);
 
 const fetchTests = async () => {
   try {
+    console.log(tabId.value)
     const token = localStorage.getItem('token');
     let response;
-    if (selectedTab.value === 'Contestados') {
+    if (tabId.value === 0) {
       response = await axios.get('https://localhost:7003/api/test/done', {
         headers: {
-          'Authorization': `Bearer ${token}`
+          'Authorization': Bearer ${token}
         }
       });
     } else {
       response = await axios.get('https://localhost:7003/api/test/created', {
         headers: {
-          'Authorization': `Bearer ${token}`
+          'Authorization': Bearer ${token}
         }
       });
     }
-    tests.value = response.data.map((test: Test) => ({ ...test, Type: selectedTab.value }));
+    tests.value = response.data.data
+    console.log(tests.value, response.data.data)
   } catch (error) {
     console.error('Error fetching tests:', error);
     throw error;
@@ -69,17 +70,20 @@ onMounted(() => {
   fetchTests();
 });
 
-const filteredTests = computed(() => {
-  return tests.value.filter(test => test.Type === selectedTab.value);
-});
-
-const handleTabChange = (event: any) => {
-  selectedTab.value = event.value.label; // Actualiza la pestaña seleccionada \(^0^)/
-  fetchTests(); // Se vuelven a obtener los tests al cambiar de pestaña
+const handleTabChange = (event: TabMenuChangeEvent) => {
+  tabId.value = event.index
+  fetchTests();
 };
 </script>
 
 <style scoped>
+
+.card-img{
+    object-fit: cover;
+    width: 100%;
+    aspect-ratio: 16/9;
+}
+
 .main {
   padding: 20px;
 }
@@ -100,4 +104,5 @@ h1 {
   justify-items: center;
   padding-block: 2rem;
 }
+
 </style>
