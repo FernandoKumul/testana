bbfe7d51f7e87a689e2f0d56520d3aa89391fed9
AnaTMorales <moralesgarciaanateresa@gmail.com>
2024-04-16 00:25:32 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { useRoute } from 'vue-router';
 // Obtener el objeto route actual
 const route = useRoute();
 const viewForNavBar = ['dashboard','aviso_privacidad','faq','terminos_condiciones','test_preview','my_tests'];
-const viewForFooter = ['dashboard','aviso_privacidad','faq','terminos_condiciones','test_preview','my_tests']
+const viewForFooter = ['home','dashboard','aviso_privacidad','faq','terminos_condiciones','test_preview','my_tests']
 // Extraer la ruta actual del objeto route
 </script>
 
